ee402dfcd7ca8a3d68a4f00ec2e320a0c2a7d9c0
Jonny <jpdadof3@gmail.com>
2019-09-06 13:48:15 -0500

    initial commit with Navbar complete

---
+++ react-clicky-game/src/components/navbar/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Nav.css";
+
+
+
+const Navbar = props => (
+    <div className="navbar">
+        <div>React Memory Game!</div>
+        <div className={props.navMsgColor}>{props.navMessage}</div>
+        <div>
+            Score: {props.score} <span className="pipe">|</span> High Score: {" "}{props.highScore}
+        </div>
+    </div>
+)
+
+export default Navbar;
+
