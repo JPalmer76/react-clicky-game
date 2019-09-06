fc3c0092e7b921975aacc615d516b24245bdfe34
Jonny <jpdadof3@gmail.com>
2019-09-06 14:48:43 -0500

    created footer, but cant get it to work

--- react-clicky-game/src/App.js
+++ react-clicky-game/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar/Nav';
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Footer/>
     </div>
   );
 }
---
+++ react-clicky-game/src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => (
+    <div>
+  <div className="footer text-center">Jon Palmer Github:</div>
+  </div>
+);
+
+export default Footer;
