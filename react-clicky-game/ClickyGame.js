330415cff7b5056dc72ae396793aeddf4b382c1d
Jonny <jpdadof3@gmail.com>
2019-09-07 14:50:03 -0500

    created Container.js code

--- react-clicky-game/ClickyGame.js
+++ react-clicky-game/ClickyGame.js
@@ -34,5 +34,73 @@ class ClickyGame extends Component {
           }
           return shuffleArr;
     }
-    
+
+    checkClicked(clickedElem) {
+        const prevState = this.state.wasClicked.slice();
+
+        const shuffled = this.shuffleArray();
+
+        let score = this.state.score;
+        let highScore = this.state.highScore;
+
+        if (!this.state.wasClicked.includes(clickedElem)) {
+            
+            if (score === highScore) {
+              score++;
+              highScore++;
+      
+             
+            } else {
+              score++;
+            }
+      
+        
+            prevState.push(clickedElem);
+          }
+          if (this.state.wasClicked.includes(clickedElem)) {
+            let score = 0;
+            return this.setState({
+              score: score,
+              highScore: highScore,
+              navMsgColor: "incorrect",
+              navMessage: "Incorrect guess!",
+              allCharacters: shuffled,
+              wasClicked: [],
+              shake: true
+            });
+          }
+          this.setState({
+            score: score,
+            highScore: highScore,
+            navMsgColor: "correct",
+            navMessage: "You Guessed Correctly!",
+            allCharacters: shuffled,
+            wasClicked: prevState,
+            shake: false
+          });
+          return setTimeout(() => this.setState({ navMsgColor: "" }), 500);
+    }
+    render() {
+        const state = this.state;
+        return (
+          <div>
+            <Navbar
+              score={state.score}
+              highScore={state.highScore}
+              navMessage={state.navMessage}
+              navMsgColor={state.navMsgColor}
+            />
+            <Banner />
+            <Container
+              shake={state.shake}
+              characters={state.allCharacters}
+              clickEvent={this.clickEvent}
+            />
+            <Footer />
+          </div>
+        );
+      }
+
 }
+
+export default ClickyGame;
