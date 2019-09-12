106fc2cb7b488592b1c6405b5774eab10985c39f
Jonny <jpdadof3@gmail.com>
2019-09-11 21:39:31 -0500

    completed game

--- react-clicky-game/src/components/navbar/Nav.js
+++ react-clicky-game/src/components/navbar/Nav.js
@@ -1,5 +1,7 @@
 import React from "react";
+import "../navbar/index";
 import "./Nav.css";
+
 
 
 
--- react-clicky-game/src/index.js
+++ react-clicky-game/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import ClickyGame from './components/ClickyGame';
+import './index.css';
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<ClickyGame />, document.getElementById("root"));
 
